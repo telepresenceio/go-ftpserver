8ec55b54d82b55da151cfe9b1bf1bd9a8a60f08b
Thomas Hallgren <thomas@tada.se>
2025-12-31 14:37:09 +0100

    Follow ftpserverlibs adaption to use log/slog and update dependencies
    Signed-off-by: Thomas Hallgren <thomas@tada.se>

--- go.mod
+++ go.mod
@@ -3,15 +3,14 @@ module github.com/telepresenceio/go-ftpserver
 go 1.24.0
 
 require (
-	github.com/fclairamb/ftpserverlib v0.27.0
-	github.com/fclairamb/go-log v0.6.0
+	github.com/fclairamb/ftpserverlib v0.28.0
 	github.com/spf13/afero v1.15.0
-	github.com/telepresenceio/dlib/v2 v2.0.1
+	github.com/telepresenceio/dlib/v2 v2.1.1
 )
 
 require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
-	golang.org/x/sys v0.38.0 // indirect
-	golang.org/x/text v0.31.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/text v0.32.0 // indirect
 )
--- logger.go
+++ logger.go
@@ -2,10 +2,9 @@ package server
 
 import (
 	"context"
-	"fmt"
+	"log/slog"
 
-	ftplog "github.com/fclairamb/go-log"
-
+	"github.com/telepresenceio/dlib/v2/dgroup"
 	"github.com/telepresenceio/dlib/v2/dlog"
 )
 
@@ -13,61 +12,43 @@ type logger struct {
 	context.Context
 }
 
-// Logger creates a github.com/fclairamb/go-log Logger that sends its output
-// to a dlog Logger
-func Logger(ctx context.Context) ftplog.Logger {
+func dlogLevel(level slog.Level) (lvl dlog.LogLevel) {
+	switch level {
+	case slog.LevelDebug:
+		lvl = dlog.LogLevelDebug
+	case slog.LevelInfo:
+		lvl = dlog.LogLevelInfo
+	case slog.LevelWarn:
+		lvl = dlog.LogLevelWarn
+	default:
+		lvl = dlog.LogLevelError
+	}
+	return lvl
+}
+
+func (d *logger) Enabled(ctx context.Context, level slog.Level) bool {
+	return dlog.MaxLogLevel(ctx) >= dlogLevel(level)
+}
+
+func (d *logger) Handle(ctx context.Context, record slog.Record) error {
+	dlog.Log(ctx, dlogLevel(record.Level), record.Message)
+	return nil
+}
+
+func (d *logger) WithAttrs(attrs []slog.Attr) slog.Handler {
+	ctx := d.Context
+	for _, attr := range attrs {
+		ctx = dlog.WithField(ctx, attr.Key, attr.Value)
+	}
 	return &logger{ctx}
 }
 
-func (d *logger) Debug(event string, keyvals ...any) {
-	d.log(dlog.LogLevelDebug, event, keyvals)
+func (d *logger) WithGroup(name string) slog.Handler {
+	return &logger{dgroup.WithGoroutineName(d.Context, name)}
 }
 
-func (d *logger) Info(event string, keyvals ...any) {
-	d.log(dlog.LogLevelInfo, event, keyvals)
-}
-
-func (d *logger) Warn(event string, keyvals ...any) {
-	d.log(dlog.LogLevelWarn, event, keyvals)
-}
-
-func (d *logger) Error(event string, keyvals ...any) {
-	d.log(dlog.LogLevelError, event, keyvals)
-}
-
-func (d *logger) Panic(event string, keyvals ...any) {
-	d.log(dlog.LogLevelError, event, keyvals)
-	panic(event)
-}
-
-func (d *logger) With(keyvals ...any) ftplog.Logger {
-	return d.with(keyvals)
-}
-
-func (d *logger) log(level dlog.LogLevel, event string, keyvals []any) {
-	if len(keyvals) > 0 {
-		// Don't create a logger with fields on the fly if the max level
-		// discards the output.
-		if dlog.MaxLogLevel(d) >= level {
-			dlog.Log(d.with(keyvals), level, event)
-		}
-	} else {
-		dlog.Log(d, level, event)
-	}
-}
-
-func (d *logger) with(keyvals []any) *logger {
-	l := len(keyvals)
-	if l == 0 {
-		return d
-	}
-	if l%2 != 0 {
-		keyvals = append(keyvals, "*** missing value ***")
-		l++
-	}
-	c := d.Context
-	for i := 0; i < l; i += 2 {
-		c = dlog.WithField(c, fmt.Sprint(keyvals[i]), keyvals[i+1])
-	}
-	return &logger{c}
+// Logger creates a github.com/fclairamb/go-log Logger that sends its output
+// to a dlog Logger
+func Logger(ctx context.Context) *slog.Logger {
+	return slog.New(&logger{ctx})
 }
